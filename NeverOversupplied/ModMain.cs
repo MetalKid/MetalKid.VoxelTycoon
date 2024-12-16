deb5dd08732cd87280af9c76d58164c2f22805f8
Dan Lorenz <metalkid007@yahoo.com>
2024-12-16 14:15:31 -0600

    New mods

---
+++ NeverOversupplied/ModMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using VoxelTycoon;
+using VoxelTycoon.Cities;
+using VoxelTycoon.Modding;
+
+namespace NeverOversupplied
+{
+    /// <summary>
+    /// This class mods the game so industry buildings never trigger Oversupplied status.
+    /// </summary>
+    public class ModMain : Mod
+    {
+        private static readonly ZeroTimeSpanCounterInt _zeroCounter = new ZeroTimeSpanCounterInt();
+        private static readonly Type _cityDemandType = typeof(CityDemand);
+
+        private readonly FieldInfo _oversupplyCounterField =
+            _cityDemandType.GetField("_oversupplyCounter", BindingFlags.Instance | BindingFlags.NonPublic);
+        
+        protected override void OnGameStarted()
+        {
+            base.OnGameStarted();
+
+            OverrideSupplyCounter();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            OverrideSupplyCounter();
+        }
+
+        private void OverrideSupplyCounter()
+        {
+            if (_oversupplyCounterField == null 
+                || CityManager.Current == null 
+                || CityManager.Current.Cities == null)
+            {
+                return;
+            }
+
+            foreach (var city in CityManager.Current.Cities.ToList())
+            {
+                if (city.Demands == null || city.Demands.Count == 0)
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < city.Demands.Count; i++)
+                {
+                    var demand = city.Demands[i];
+                    _oversupplyCounterField.SetValue(demand, _zeroCounter);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Overrides base class to essentially ignore all AddValue calls.
+    /// </summary>
+    public class ZeroTimeSpanCounterInt : TimeSpanCounterInt
+    {
+        protected override int AddValue(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ UnlimitedStorage/ModMain.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using VoxelTycoon.Cities;
+using VoxelTycoon.Modding;
+
+namespace UnlimitedStorage
+{
+    /// <summary>
+    /// This class mods the game so industry buildings have virtually unlimited storage.
+    /// </summary>
+    public class ModMain : Mod
+    {
+        protected override void OnGameStarted()
+        {
+            base.OnGameStarted();
+
+            try
+            {
+                var harmony = new Harmony("com.github.metalcore.voxeltycoon.unlimitedstorage");
+                harmony.PatchAll(typeof(ModMain).Assembly);
+            }
+            catch { }
+        }
+    }
+
+    [HarmonyPatch(typeof(CityDemand))]
+    [HarmonyPatch(nameof(CityDemand.Capacity), MethodType.Getter)]
+    public class CityDemandPatchForCapacity
+    {
+        [HarmonyPostfix]
+        public static void CapacityPostfix(ref int __result, CityDemand __instance)
+        {
+            __result = 999_999_990;
+        }
+    }
+}
